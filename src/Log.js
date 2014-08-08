56538871575160d2d2950592361d36b006e8d021
M.R.Z <zgd1348833@gmail.com>
2014-08-08 10:28:27 +0800

    Add timetag for log display

--- src/Log.js
+++ src/Log.js
@@ -1,14 +1,39 @@
+/**
+ * This is a Log class for format display log system firendly
+ */
+function keep2(v){
+	return ((v)>9?(v):("0"+(v)));
+}
+
+function timeTag(){
+	var tmp = new Date();
+	return (tmp.getFullYear()
+				+ "." + keep2(tmp.getMonth()+1)
+				+ "." + keep2(tmp.getDate())
+				+ " " + keep2(tmp.getHours())
+				+ ":" + keep2(tmp.getMinutes())
+				+ ":" + keep2(tmp.getSeconds())
+				+ "." + tmp.getMilliseconds());
+}
+
 function v(tag, msg){
-	console.log("V: " + tag + ": " + msg);
+	if(isDebug())
+		console.log(timeTag() + " V: " + tag + ": " + msg);
 }
+
 function d(tag, msg){
-	console.log("D: " + tag + ": " + msg);
+	if(isDebug())
+		console.log(timeTag() + " D: " + tag + ": " + msg);
 }
+
 function w(tag, msg){
-	console.log("W: " + tag + ": " + msg);
+	if(isDebug())
+		console.log(timeTag() + " W: " + tag + ": " + msg);
 }
+
 function e(tag, msg){
-	console.log("E: " + tag + ": " + msg);
+	if(isDebug())
+		console.log(timeTag() + " E: " + tag + ": " + msg);
 }
 
 function isDebug(){
@@ -19,5 +44,6 @@ exports.v = v;
 exports.d = d;
 exports.w = w;
 exports.e = e;
+exports.timeTag = timeTag;
 exports.isDebug = isDebug;
 
