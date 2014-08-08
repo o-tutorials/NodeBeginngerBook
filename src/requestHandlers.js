c50d49a6e00d65bd8b4e473736ac4c15bbe30247
M.R.Z <zgd1348833@gmail.com>
2014-08-08 10:31:04 +0800

    Add 4.10 sample test

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -2,7 +2,13 @@ var Log = require("./Log");
 var TAG = "requestHandlers";
 
 function start(){
+	function sleep(milliSeconds){
+		Log.d(TAG, "sleep " + milliSeconds + " milliSeconds.");
+		var startTime = new Date().getTime();
+		while(new Date().getTime() < startTime + milliSeconds);
+	}
 	Log.d(TAG, "Request handler start was called.");
+	sleep(10000);
 	return "start - Hello, world.";
 }
 
